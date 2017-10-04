a624b9604410bb7d82d835e4284f7a8d7baa5f5d
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-04 16:08:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  //var newLine = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    if (katzDeliLine.length < 1) {
+      katzDeli.push(name);
+      return katzDeli;
+    } else {
+    katzDeliLine.push(name);
+    return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+   }
+  }
+}
+
+// function currentLine(katzDeliLine) {
+//   for (var i = 0; i < katzDeliLine.length; i++) {
+//     if (katzDeliLine.length < 1) {
+//       return "The line is currently empty."
+//     } else {
+//     return ("The line is currently: " + (i + 1) + ". " + katzDeliLine[i])
+//   }
+//  }
+// }
