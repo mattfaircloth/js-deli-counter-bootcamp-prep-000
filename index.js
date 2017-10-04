b0c96fccc4214aae1e9370c3ad46ab57e484ee11
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-04 16:31:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function takeANumber(katzDeliLine, name) {
 
 function currentLine(line) {
     var lineArray = [];
-    for (var i = 0; i < line.length; i++) {
-      if (line.length === 0) {
-        return "The line is currently empty."
-      } else {
-        lineArray.push((i + 1) + ". " + line[i])
+    if (line.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for (var i = 0; i < line.length; i++) {
+        lineArray.push((i + 1) + ". " + line[i] + "")
       }
     }
     return "The line is currently: " + lineArray;
