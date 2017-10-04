b295515628875c55ec22524b02b70608164523de
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-04 16:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function takeANumber(katzDeliLine, name) {
 
 
 function currentLine(katzDeliLine) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
+  //or (var i = 0; i < katzDeliLine.length; i++) {
     if (katzDeliLine.length === 0) {
       return "The line is currently empty."
     } else {
     return ("The line is currently: " + (i + 1) + ". " + katzDeliLine[i])
   }
- }
+ //}
 }
