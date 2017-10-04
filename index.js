b431f8fa7714c471d34709b89a49055ac5745a4c
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-04 16:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  //var newLine = [];
+  katzDeli.push(name);
   for (var i = 0; i < katzDeliLine.length; i++) {
-    if (katzDeliLine.length < 1) {
-      katzDeli.push(name);
-      return katzDeli;
-    } else {
+    // if (katzDeliLine.length < 1) {
+    //   katzDeli.push(name);
+    //   return katzDeli;
+    // } else {
     katzDeliLine.push(name);
     return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
    }
   }
-}
+
 
 // function currentLine(katzDeliLine) {
 //   for (var i = 0; i < katzDeliLine.length; i++) {
